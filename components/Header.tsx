bfa57a498acb096f9357dcfb361724672abcf1e1
TuPM <tupm.citgroup@gmail.com>
2025-10-09 16:23:02 +0700

    add

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
       <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('https://picsum.photos/1200/400?image=1060')" }}></div>
       <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-black/30"></div>
       <div className="relative h-full flex items-center justify-center text-white px-0 sm:px-0">
-        <img src="/logo.png" alt="Gold One Logo" className="w-14 h-14 flex-shrink-0" />
+        <img src="/banghieu.png" alt="Gold One Logo" className="h-14 flex-shrink-0" />
       </div>
     </header>
   );
