733f2cca60f905bec2c72cf0751f5911ef36a1f4
Tu Pham <minhtu.bk14@gmail.com>
2025-10-16 13:58:16 +0700

    add

--- components/AquariumBackground.tsx
+++ components/AquariumBackground.tsx
@@ -118,8 +118,6 @@ const AquariumBackground: React.FC<Props> = ({
                 overflow: "hidden",
                 zIndex: 0,
                 pointerEvents: "none",
-                background:
-                    "linear-gradient(180deg, rgba(2,6,23,1) 0%, rgba(15,23,42,1) 35%, rgba(8,47,73,1) 100%)",
             }}
         >
             {/* layer cá/cua */}
